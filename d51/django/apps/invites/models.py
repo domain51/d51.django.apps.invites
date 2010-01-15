b4812b52c6f3c3a995b4749d09692c7cec9a4799
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-01-15 16:57:11 -0600

    continuing to refactor...

--- d51/django/apps/invites/models.py
+++ d51/django/apps/invites/models.py
@@ -3,12 +3,24 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse 
 from django.template.defaultfilters import slugify
 
+class InvitationManager(models.Manager):
+    def accept_invite(self, invitation, user):
+        invitation.resulting_user = user
+        invitation.status = 1
+        invitation.save()
+
+    def create_for(self, backend_string, user, with_form):
+        return [self.create(**{
+                'target':id,
+                'status':0,
+                'backend':backend_string,
+                'sent_by':user,
+            }) for id in with_form.invite_ids]
+
 class Invitation(models.Model):
     STATUS_CHOICES = (
-        (0, 'Unsent'),
-        (1, 'Pending'),
-        (2, 'Accepted'),
-        (3, 'Expired'),
+        (0, 'Pending'),
+        (1, 'Accepted'),
     )
     sent_by = models.ForeignKey(User, related_name='invitations')
     backend = models.CharField(max_length=255)
@@ -16,6 +28,8 @@ class Invitation(models.Model):
     status = models.PositiveIntegerField(choices=STATUS_CHOICES)
     published = models.DateTimeField(auto_now_add=True)
     resulting_user = models.ForeignKey(User, null=True, blank=True)
+
+    objects = InvitationManager()
 
     def get_backend(self):
         from .backends import invite_site 
---
+++ d51/django/apps/invites/utils.py
@@ -0,0 +1,6 @@
+class InviteBackendException(Exception):
+    pass
+
+INVITE_SESSION_KEY = 'd51.django.apps.invites.INVITE_SESSION_KEY'
+SENT_INVITATIONS = 'sent_invitations'
+
